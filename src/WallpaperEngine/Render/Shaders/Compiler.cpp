986e390955f4d4bf473cd0c10405a033238268b0
LongLeCE <siromansiroas@gmail.com>
2024-07-16 23:19:43 +0800

    debug

--- src/WallpaperEngine/Render/Shaders/Compiler.cpp
+++ src/WallpaperEngine/Render/Shaders/Compiler.cpp
@@ -612,8 +612,9 @@ void Compiler::parseComboConfiguration (const std::string& content, int defaultV
     // add line feed just in case
     this->m_compiledContent += "\n";
 
-    std::cout << "type: " << type << "\n";
-    std::cout << "entry: " << combo->get<std::string> () << "\n";
+    std::cout << "type: " << *type << "\n";
+    std::cout << "entry: " << *combo->get<std::string> () << "\n";
+    std::cout << "combo: " << *this->m_combos->end () << "\n";
 
     // check the combos
     const auto entry = this->m_combos->find (combo->get<std::string> ());
